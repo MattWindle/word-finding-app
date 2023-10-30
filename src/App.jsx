10e3a9be98db9b1fcd4636abd57c15738c97cf86
Matt Windle <matthewwindle@ekm.com>
2023-10-30 20:37:53 +0000

    Change title into a label

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,11 @@ function App() {
     <>
       <div className="bg-gray-700/70 h-[100svh] flex items-center">
         <div className="container p-4  mx-auto flex flex-col items-center pt-8">
-          <h1 className="text-white font-semibold text-5xl pb-2 ">
-            Word Finding App
-          </h1>
           <form action="" className="mt-4"  onSubmit={searchWord}>
-            <label htmlFor="searchWord" className="mr-4 text-gray-200 sr-only">
+            <label htmlFor="searchWord" className="text-white font-semibold text-5xl pb-2 text-center block mb-4">
               Search Word
             </label>
+            <div>
             <input
               value={word}
               onChange={(e) => setWord(e.target.value)}
@@ -39,9 +37,13 @@ function App() {
             <button className="ml-4 py-2 px-4 rounded-md border-2 border-gray-200  cursor-pointer font-medium text-white hover:bg-white/20">
               Search
             </button>
+            </div>
           </form>
           {isLoading ? (
-            <p className="text-white pt-8 text-xl">Is loading...</p>
+            <p onClick={() => {
+                  getWords(singleWordResult.word, searchType)
+                  setWord(singleWordResult.word)
+                }} key={index} className="bg-gray-200 py-2 px-4 capitalize rounded-md hover:bg-blue-200 cursor-pointer">{singleWordResult.word}</p>
           ) : (
             <div className="flex flex-row gap-4 max-w-[1200px] flex-wrap mt-8">
               {wordResults.map((singleWordResult, index) => (
